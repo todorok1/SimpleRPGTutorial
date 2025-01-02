c426012913b1d86c051904af0d30bd8b91af8f19
todorok1 <ttodoroki@ekulabo.com>
2025-01-02 17:29:10 +0900

    チュートリアル第7回
    - カメラがキャラクターを追従する機能を実装

---
+++ Assets/Scripts/Consts/ObjectTagSettings.cs
@@ -0,0 +1,13 @@
+namespace SimpleRpg
+{
+    /// <summary>
+    /// タグに関する定義値を保持するクラスです。
+    /// </summary>
+    public static class ObjectTagSettings
+    {
+        /// <summary>
+        /// 操作キャラクターのタグ名です。
+        /// </summary>
+        public static readonly string Player = "Player";
+    }
+}
