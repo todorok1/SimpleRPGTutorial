076a14a036ea9d9e478fa7c08dabb82c415d439f
todorok1 <ttodoroki@ekulabo.com>
2025-02-19 17:14:24 +0900

    チュートリアル第21回
    - 戦闘の各フェーズの定義を作成した
    - ゲーム全体の状態の定義を作成した

---
+++ Assets/Scripts/Enums/GameState.cs
@@ -0,0 +1,13 @@
+namespace SimpleRpg
+{
+    /// <summary>
+    /// ゲーム全体の状態に関する状態を定義する列挙型です。
+    /// </summary>
+    public enum GameState
+    {
+        None,
+        Moving,
+        Battle,
+        Event,
+    }
+}
