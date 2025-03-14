c18a52f03b03784b563f03503a80937305d316c1
todorok1 <ttodoroki@ekulabo.com>
2025-03-14 20:54:15 +0900

    チュートリアル第45回
    - 選択ウィンドウの機能のうち、UIに関する部分を実装

---
+++ Assets/Scripts/Consts/SelectionItemPosition.cs
@@ -0,0 +1,28 @@
+namespace SimpleRpg
+{
+    /// <summary>
+    /// 選択ウィンドウの各項目の位置の定義です。
+    /// </summary>
+    public static class SelectionItemPosition
+    {
+        /// <summary>
+        /// 左上の項目のインデックスです。
+        /// </summary>
+        public const int LeftTop = 0;
+
+        /// <summary>
+        /// 右上の項目のインデックスです。
+        /// </summary>
+        public const int RightTop = 1;
+
+        /// <summary>
+        /// 左下の項目のインデックスです。
+        /// </summary>
+        public const int LeftBottom = 2;
+
+        /// <summary>
+        /// 右下の項目のインデックスです。
+        /// </summary>
+        public const int RightBottom = 3;
+    }
+}
