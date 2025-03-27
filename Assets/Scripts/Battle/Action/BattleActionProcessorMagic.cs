f7ed957fff29074128b3399a1420156cec9d471a
todorok1 <ttodoroki@ekulabo.com>
2025-03-27 15:57:41 +0900

    チュートリアル第55回
    - 魔法に関する行動処理を行うクラスを追加

---
+++ Assets/Scripts/Battle/Action/BattleActionProcessorMagic.cs
@@ -0,0 +1,171 @@
+using System.Collections;
+using UnityEngine;
+
+namespace SimpleRpg
+{
+    /// <summary>
+    /// 戦闘中の魔法アクションを処理するクラスです。
+    /// </summary>
+    public class BattleActionProcessorMagic : MonoBehaviour, IBattleActionProcessor
+    {
+        /// <summary>
+        /// 戦闘中のアクションを処理するクラスへの参照です。
+        /// </summary>
+        BattleActionProcessor _actionProcessor;
+
+        /// <summary>
+        /// 戦闘に関する機能を管理するクラスへの参照です。
+        /// </summary>
+        BattleManager _battleManager;
+
+        /// <summary>
+        /// メッセージウィンドウを制御するクラスへの参照です。
+        /// </summary>
+        MessageWindowController _messageWindowController;
+
+        /// <summary>
+        /// 戦闘中の敵キャラクターの管理を行うクラスへの参照です。
+        /// </summary>
+        EnemyStatusManager _enemyStatusManager;
+
+        /// <summary>
+        /// 魔法効果をポーズするかどうかのフラグです。
+        /// </summary>
+        bool _pauseMagicEffect;
+
+        /// <summary>
+        /// 参照をセットします。
+        /// </summary>
+        public void SetReferences(BattleManager battleManager, BattleActionProcessor actionProcessor)
+        {
+            _battleManager = battleManager;
+            _actionProcessor = actionProcessor;
+            _messageWindowController = _battleManager.GetWindowManager().GetMessageWindowController();
+            _enemyStatusManager = _battleManager.GetEnemyStatusManager();
+        }
+
+        /// <summary>
+        /// 魔法のアクションを処理します。
+        /// </summary>
+        public void ProcessAction(BattleAction action)
+        {
+            var magicData = MagicDataManager.GetMagicDataById(action.itemId);
+
+            // 消費MPの分だけMPを減らします。
+            int hpDelta = 0;
+            int mpDelta = magicData.cost * -1;
+            if (action.isActorFriend)
+            {
+                CharacterStatusManager.ChangeCharacterStatus(action.actorId, hpDelta, mpDelta);
+            }
+            else
+            {
+                _enemyStatusManager.ChangeEnemyStatus(action.actorId, hpDelta, mpDelta);
+            }
+
+            _actionProcessor.SetPauseProcess(true);
+            StartCoroutine(ProcessMagicActionCoroutine(action, magicData));
+        }
+
+        /// <summary>
+        /// 魔法のアクションを処理するコルーチンです。
+        /// </summary>
+        IEnumerator ProcessMagicActionCoroutine(BattleAction action, MagicData magicData)
+        {
+            // 魔法の効果を処理します。
+            foreach (var magicEffect in magicData.magicEffects)
+            {
+                // メッセージ表示用の行動を生成します。
+                BattleAction messageAction = new()
+                {
+                    actorId = action.actorId,
+                    targetId = action.targetId,
+                    isActorFriend = action.isActorFriend,
+                    isTargetFriend = action.isTargetFriend
+                };
+
+                if (magicEffect.magicCategory == MagicCategory.Recovery)
+                {
+                    int hpDelta = BattleCalculator.CalculateHealValue(magicEffect.value);
+                    int mpDelta = 0;
+                    bool isMagicTargetFriend = IsMagicTargetFriend(magicEffect);
+                    if (isMagicTargetFriend)
+                    {
+                        messageAction.targetId = action.actorId;
+                        messageAction.isTargetFriend = action.isActorFriend;
+                    }
+                    else
+                    {
+                        messageAction.targetId = action.targetId;
+                        messageAction.isTargetFriend = !action.isActorFriend;
+                    }
+
+                    if (messageAction.isTargetFriend)
+                    {
+                        CharacterStatusManager.ChangeCharacterStatus(messageAction.targetId, hpDelta, mpDelta);
+                    }
+                    else
+                    {
+                        _enemyStatusManager.ChangeEnemyStatus(messageAction.targetId, hpDelta, mpDelta);
+                    }
+
+                    _pauseMagicEffect = true;
+                    StartCoroutine(ShowMagicHealMessage(messageAction, magicData.magicName, hpDelta));
+                }
+                else
+                {
+                    Debug.LogWarning($"未定義の魔法効果です。 ID: {magicData.magicId}");
+                }
+
+                while (_pauseMagicEffect)
+                {
+                    yield return null;
+                }
+            }
+
+            _actionProcessor.SetPauseProcess(false);
+        }
+
+        /// <summary>
+        /// 回復魔法のメッセージを表示します。
+        /// </summary>
+        IEnumerator ShowMagicHealMessage(BattleAction action, string magicName, int healValue)
+        {
+            string actorName = _actionProcessor.GetCharacterName(action.actorId, action.isActorFriend);
+            string targetName = _actionProcessor.GetCharacterName(action.targetId, action.isTargetFriend);
+
+            _actionProcessor.SetPauseMessage(true);
+            _messageWindowController.GenerateMagicCastMessage(actorName, magicName);
+            while (_actionProcessor.IsPausedMessage)
+            {
+                yield return null;
+            }
+
+            _actionProcessor.SetPauseMessage(true);
+            _messageWindowController.GenerateHpHealMessage(targetName, healValue);
+            _battleManager.OnUpdateStatus();
+            while (_actionProcessor.IsPausedMessage)
+            {
+                yield return null;
+            }
+
+            _pauseMagicEffect = false;
+        }
+
+        /// <summary>
+        /// 魔法の対象が行動者の味方かどうかを判定します。
+        /// </summary>
+        /// <param name="magicEffect">魔法効果</param>
+        bool IsMagicTargetFriend(MagicEffect magicEffect)
+        {
+            bool isFriend = false;
+            if (magicEffect.effectTarget == EffectTarget.Own
+                || magicEffect.effectTarget == EffectTarget.FriendSolo
+                || magicEffect.effectTarget == EffectTarget.FriendAll)
+            {
+                isFriend = true;
+            }
+            return isFriend;
+        }
+    }
+}
